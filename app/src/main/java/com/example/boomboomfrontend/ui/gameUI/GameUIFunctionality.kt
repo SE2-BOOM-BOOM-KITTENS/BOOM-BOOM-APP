7536982084f82c1cfebc57b1b26d3904db7a15ac
filipped <82341252+FilippEder@users.noreply.github.com>
2025-06-24 23:38:23 +0200

    Simplify playCard to pass only one card instead of list

--- app/src/main/java/com/example/boomboomfrontend/ui/gameUI/GameUIFunctionality.kt
+++ app/src/main/java/com/example/boomboomfrontend/ui/gameUI/GameUIFunctionality.kt
@@ -5,12 +5,8 @@ import com.example.boomboomfrontend.model.CardType
 import com.example.boomboomfrontend.viewmodel.gameState.GameStateViewModel
 
 fun playCard(viewModel: GameStateViewModel, cardname: String, cardtype: CardType) {
-    val cardsPlayed = Card(cardname, cardtype)
-    val cardList: MutableList<Card> = mutableListOf()
-    cardList.add(cardsPlayed)
-
-
-    viewModel.playCard(cardList)
+    val cardPlayed = Card(cardname, cardtype)
+    viewModel.playCard(cardPlayed)
 }
 
 fun passTurn(viewModel: GameStateViewModel){
