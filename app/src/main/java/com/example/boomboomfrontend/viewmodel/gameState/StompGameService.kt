b4101470dbeb0fca48749cd4e304053e092034ee
Yü Li Chang Lin <40342792+Yuelichang@users.noreply.github.com>
2025-06-25 14:54:00 +0200

    did the fixme

--- app/src/main/java/com/example/boomboomfrontend/viewmodel/gameState/StompGameService.kt
+++ app/src/main/java/com/example/boomboomfrontend/viewmodel/gameState/StompGameService.kt
@@ -1,15 +1,14 @@
 package com.example.boomboomfrontend.viewmodel.gameState
 
-import android.util.Log
 import com.example.boomboomfrontend.model.Card
 import com.example.boomboomfrontend.network.messages.PlayerMessage
 import com.example.boomboomfrontend.network.websocket.Callbacks
 import com.example.boomboomfrontend.network.websocket.Stomp
 
-class StompGameService(callbacks: Callbacks) {
-    // fixme instead of passing this whole object just pass the callback method (after annotating the interface as functional)
+class StompGameService(callback: (String) -> Unit) {
+
     init {
-        Stomp.setCallbacks(callbacks)
+        Stomp.setCallbacks(Callbacks { res -> callback(res) })
     }
 
     suspend fun disconnect(){
