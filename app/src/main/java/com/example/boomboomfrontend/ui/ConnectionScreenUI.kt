03e6d18e749b7c5a8df7564a80997aff2c8bfb7f
DAAAAANIIIIIIIIII <73837003+D4NIFAKTOR1ELLE@users.noreply.github.com>
2025-06-25 12:20:38 +0200

    Connect screen, again

--- app/src/main/java/com/example/boomboomfrontend/ui/ConnectionScreenUI.kt
+++ app/src/main/java/com/example/boomboomfrontend/ui/ConnectionScreenUI.kt
@@ -1,28 +1,51 @@
 package com.example.boomboomfrontend.ui
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.boomboomfrontend.R
+import com.example.boomboomfrontend.ui.gameUI.background
 import com.example.boomboomfrontend.viewmodel.PlayerViewModel
 import com.example.boomboomfrontend.viewmodel.lobby.LobbyViewModel
 import java.util.UUID
 
+@Preview(
+    showSystemUi = true,
+    device = "spec:width=411dp,height=891dp,dpi=420,isRound=false,chinSize=0dp,orientation=landscape"
+)
 @Composable
 fun ConnectionScreen(
-    navController: NavController,
-    onEnterGameScreen: () -> Unit,
+    navController: NavController = rememberNavController(),
+    onEnterGameScreen: () -> Unit = { },
     playerViewModel: PlayerViewModel = viewModel(),
     lobbyViewModel: LobbyViewModel = viewModel()
 ) {
@@ -39,8 +62,14 @@ fun ConnectionScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .background(Color(background))
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.gradientbackground),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxSize()
+        )
 
         Row(Modifier.fillMaxSize()) {
             Column(
@@ -92,7 +121,7 @@ fun ConnectionScreen(
                         }
                     }
                 } else {
-                    Text("No lobbies found. Tap \"Fetch Lobbies\" to refresh.")
+                    Text("No lobbies found.\nTap \"Fetch Lobbies\" to refresh!")
                 }
             }
 
